59dd1c5a6c5755c627e4bd9f4a52adffa305d5f0
Abdelhamid Maaira <agadir.9ine98@gmail.com>
2023-10-02 17:26:59 -0700

    lists

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,5 +1,13 @@
 #include "lists.h"
 
+/**
+  * check_cycle - checks if a linked list has a cycle in it.
+  *
+  * @list: pointer to the head of the linked list.
+  *
+  * Return: 1 if there is a cycle, 0 otherwise
+  */
+
 int check_cycle(listint_t *list)
 {
 	listint_t *slw = list, *fst = list;
