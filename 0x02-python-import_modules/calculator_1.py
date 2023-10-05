dfb957bef71a6daf7ee5057f24b45d6b42d6e736
Abdelhamid Maaira <agadir.9ine98@gmail.com>
2023-10-05 14:29:52 -0700

    calculator_1

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """Addition function
+
+    Args:
+        a: first parameter
+        b: second parameter
+
+    Returns:
+        The sum value of a + b
+    """
+return (a + b)
+
+
+def sub(a, b):
+    """Subtraction function
+
+    Args:
+        a: first parameter
+        b: second parameter
+
+    Returns:
+        The sub value of a - b
+    """
+return (a - b)
+
+
+def mul(a, b):
+    """Multiplication function
+
+    Args:
+        a: first parameter
+        b: second parameter
+
+    Returns:
+        The mul value of a * b
+    """
+return (a * b)
+
+
+def div(a, b):
+    """Division function
+
+    Args:
+        a: first parameter
+        b: second parameter
+
+    Returns:
+        The div value of a / b
+    """
+return int(a / b)
