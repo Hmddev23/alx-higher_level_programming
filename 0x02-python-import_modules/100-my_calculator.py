a75984bb45c663f2f80ab31f3ad55e34db1d0f8c
Abdelhamid Maaira <agadir.9ine98@gmail.com>
2023-10-05 14:09:22 -0700

    my calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,27 +1,23 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
-import sys
-from calculator_1 import add, sub, mul, div
+if __name__ == '__main__':
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
 
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+        exit(1)
 
-    a = int(sys.argv[1])
-    op = sys.argv[2]
-    b = int(sys.argv[3])
+    ops = {"+": add, "-": sub, '*': mul, "/": div}
 
-    if op == '+':
-        result = add(a, b)
-    elif op == '-':
-        result = sub(a, b)
-    elif op == '*':
-        result = mul(a, b)
-    elif op == '/':
-        result = div(a, b)
-    else:
+    if argv[2] not in ops:
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+        exit(1)
 
-    print(f"{a} {op} {b} = {result}")
+    a = int(argv[1])
+    b = int(argv[3])
+    op = argv[2]
+
+    result = ops[op](a, b)
+
+    print("{} {} {} = {}".format(a, op, b, result))
