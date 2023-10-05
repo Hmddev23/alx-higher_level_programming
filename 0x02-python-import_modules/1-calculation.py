71215b63df118435da2efea97a9f9787f6588bc6
Abdelhamid Maaira <agadir.9ine98@gmail.com>
2023-10-05 13:33:07 -0700

    calculations

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+
+    add_res = add(a, b)
+    sub_res = sub(a, b)
+    mul_res = mul(a, b)
+    div_res = div(a, b)
+
+    print("{} + {} = {}".format(a, b, add_res))
+    print("{} - {} = {}".format(a, b, sub_res))
+    print("{} * {} = {}".format(a, b, mul_res))
+    print("{} / {} = {}".format(a, b, div_res))
