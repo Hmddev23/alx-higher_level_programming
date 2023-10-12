b4880b8fd453c4229b6b1d3f4b12d2bc8e23a89e
Abdelhamid Maaira <agadir.9ine98@gmail.com>
2023-10-12 11:33:22 -0700

    fixing

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    fnl_matrix = [[0 for _ in range(len(row))] for row in matrix]
-
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            fnl_matrix[i][j] = matrix[i][j] ** 2
-
-    return (fnl_matrix)
+    tmp_matrix = []
+    for x in matrix:
+        tmp_matrix.append(list(map(lambda x: x**2, x)))
+    return (tmp_matrix)
