d023e23fcf1cdedf5b1280b39b31a88064054fe6
Hmddev23 <hamid.maira709@gmail.com>
2023-10-23 17:25:08 -0700

    safe function

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,7 +4,9 @@ import sys
 
 def safe_function(fct, *args):
     try:
-        return fct(*args)
-    except (ZeroDivisionError, ValueError, TypeError, IndexError) as error:
-        print("Exception: {}".format(error), file=sys.stderr)
+        res = fct(*args)
+    except Exception:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return None
+    else:
+        return res
