012c696961899bc4d25d72782816601d091b0b4d
Hmddev23 <hamid.maira709@gmail.com>
2023-10-24 18:53:23 -0700

    squares 4

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """define class Square constructor"""
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         """initialization of the new square
 
         Args:
