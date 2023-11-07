da618422853c88a301cd117916c93c4e6e32072c
Hmddev23 <hamid.maira709@gmail.com>
2023-11-06 17:40:27 -0800

    my list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""1-my_list.py"""
+
+
+class MyList(list):
+"""Subclass of the list class with an additional methods"""
+
+    def print_sorted(self):
+        """
+        Prints the list in sorted (ascending) order.
+        """
+        print(sorted(self))
