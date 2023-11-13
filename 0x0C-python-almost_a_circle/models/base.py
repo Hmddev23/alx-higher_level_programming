eb40a3071b44b87650bbd3a96755db392b70bc27
Hmddev23 <hamid.maira709@gmail.com>
2023-11-13 14:54:16 -0800

    test 8

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -99,7 +99,7 @@ class Base:
 
     @classmethod
     def save_to_file_csv(cls, list_objs):
-        """write the CSV serialization of a list of objects to a file"""
+        """write the CSV serialization"""
         from models.rectangle import Rectangle
         from models.square import Square
         if list_objs is not None:
@@ -116,7 +116,7 @@ class Base:
 
     @classmethod
     def load_from_file_csv(cls):
-        """return the list of classes instantiated from a CSV file"""
+        """return a list of class instances from a CSV file"""
         from models.rectangle import Rectangle
         from models.square import Square
         ret = []
