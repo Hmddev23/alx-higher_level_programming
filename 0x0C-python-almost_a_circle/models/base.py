4ec0e57a38c6f94cfee42783150e91021a8c2be2
Hmddev23 <hamid.maira709@gmail.com>
2023-11-13 13:51:13 -0800

    base 2

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,3 +24,28 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """return the serialization JSON of a dicts list.
+
+        Args:
+            list_dictionaries (list): List of dictionaries.
+        """
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        else:
+            return dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """return the deserialization of a JSON string.
+
+        Args:
+            json_string (str): JSON string representation of a list of dictionaries.
+        Returns:
+            None or empty list.
+        """
+        if json_string is None or not json_string:
+            return []
+        return loads(json_string)
