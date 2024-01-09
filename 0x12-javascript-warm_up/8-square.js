2405476969f2d9ae4ee5791a7e6d9c42f608f93d
Hmddev23 <hamid.maira709@gmail.com>
2024-01-08 17:30:53 -0800

    13 tasks

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-const square_size = Math.floor(Number(process.argv[2]));
-if (isNaN(square_size)) {
+const square = Math.floor(Number(process.argv[2]));
+if (isNaN(square)) {
   console.log('Missing size');
 } else {
-  for (let r = 0; r < square_size; r++) {
+  for (let r = 0; r < square; r++) {
     let row = '';
-    for (let c = 0; c < square_size; c++) row += 'X';
+    for (let c = 0; c < square; c++) row += 'X';
     console.log(row);
   }
 }
