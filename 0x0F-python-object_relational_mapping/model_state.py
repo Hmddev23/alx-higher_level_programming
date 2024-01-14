208a0c6677d38d1d0d3bf9e5f17b8973ff72d406
Hmddev23 <hamid.maira709@gmail.com>
2024-01-14 15:19:30 -0800

    task-6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,8 +7,8 @@ the Base class from MySQLAlchemy ORM.
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-
 Base = declarative_base()
+
 
 class State(Base):
     """
