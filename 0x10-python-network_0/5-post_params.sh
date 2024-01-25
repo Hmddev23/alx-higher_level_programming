484ea4008f4b1db2ebc95840eb4e41cd2f77f9f7
Hmddev23 <hamid.maira709@gmail.com>
2024-01-25 12:54:55 -0800

    fix

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# send a POST request to the URL with specified parameters
-# and display the body of the response.
+# send a POST request and display the body of the response.
 curl -sL "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
